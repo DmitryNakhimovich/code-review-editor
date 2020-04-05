deef71ac3946a8b938898f2c905792b2f276df2c
Dmitriy Nakhimovich <Dmitriy.Nahimovich@artezio.com>
2020-04-05 22:35:13 +0300

    update package

--- src/redux/actions/posts.ts
+++ src/redux/actions/posts.ts
@@ -1,41 +1,11 @@
-import faker from 'faker';
 import { FETCH_POSTS, RECEIVE_POSTS } from '../types/posts';
-
-const {
-  lorem: { paragraph, paragraphs, words },
-  random: { uuid },
-} = faker;
-
-export const receivePosts = (
-  count = 1,
-  options = {
-    id: '',
-  },
-) => {
-  const posts = Array(count)
-    .fill({})
-    .map(() => {
-      const summary = paragraph();
-      return {
-        id: options.id || uuid().split('-')[0],
-        title: words(),
-        summary,
-        content: `${summary}\n${paragraphs()}`,
-      };
-    });
-
-  return {
-    type: RECEIVE_POSTS,
-    posts,
-  };
-};
 
 const POST_COUNT = 5;
 export const fetchPosts = () => {
   return (dispatch: any) => {
     // emulate api request
     dispatch({ type: FETCH_POSTS });
-    setTimeout(() => dispatch(receivePosts(POST_COUNT)), 1000);
+    // setTimeout(() => dispatch(receivePosts(POST_COUNT)), 1000);
   };
 };
 
@@ -43,6 +13,6 @@ export const fetchPost = (id: string) => {
   return (dispatch: any) => {
     // emulate api request
     dispatch({ type: FETCH_POSTS });
-    setTimeout(() => dispatch(receivePosts(1, { id })), 1000);
+    // setTimeout(() => dispatch(receivePosts(1, { id })), 1000);
   };
 };
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,6 @@
 import { Action, applyMiddleware, createStore, Store } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-import { createLogger } from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { reducer } from './root';
 
-export const store: Store<{}, Action> = createStore(
-  reducer,
-  composeWithDevTools(applyMiddleware(thunkMiddleware, createLogger())),
-);
+export const store: Store<{}, Action> = createStore(reducer, composeWithDevTools(applyMiddleware(thunkMiddleware)));
